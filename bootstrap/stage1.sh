c9a2e22eef95a0337d8ee1dfc441ffa813240bbb
Nathan Flynn <nathan.flynn@zeninternet.co.uk>
2013-07-08 19:56:49 +0100

    Added new modules

--- bootstrap/stage1.sh
+++ bootstrap/stage1.sh
@@ -26,3 +26,24 @@ if ! test -e /usr/bin/puppet; then
 else
     echo puppet is already installed
 fi
+
+if ! test -d /etc/puppet/modules/vcsa; then
+    echo Installing vcsa module
+    puppet module install vmware/vcsa
+else
+    echo already installed  vmware/vcsa
+fi
+
+if ! test -d /etc/puppet/modules/vcenter; then
+    echo Installing vcenter module
+    puppet module install vmware/vcenter
+else
+    echo already installed  vmware/vcenter
+fi
+
+if ! test -d /etc/puppet/modules/vshield; then
+    echo Installing vshield module
+    puppet module install vmware/vshield
+else
+    echo already installed  vmware/vshield
+fi
